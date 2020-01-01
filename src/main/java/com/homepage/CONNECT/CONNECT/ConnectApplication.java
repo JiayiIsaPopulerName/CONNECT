2682891008c1e2c5dafe13f619253bddc5c7bc65
apple <apple@appledeMacBook-Pro-2.local>
2020-01-01 20:33:46 +0300

    first commit

---
+++ src/main/java/com/homepage/CONNECT/CONNECT/ConnectApplication.java
@@ -0,0 +1,13 @@
+package com.homepage.CONNECT.CONNECT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homepage/CONNECT/CONNECT/ConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homepage.CONNECT.CONNECT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
